66f80148714387b64f0f4dbff65f85de668fe7e3
luuthanhtu <hoangtusocolakuteboy@gmail.com>
2024-01-17 21:14:24 +0700

    Update install-docker-amazon.sh

--- linux_install_guilde_docker/install-docker-amazon.sh
+++ linux_install_guilde_docker/install-docker-amazon.sh
@@ -6,4 +6,6 @@ sudo systemctl enable docker
 newgrp docker
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-sudo usermod -aG docker $USER
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+sudo chmod 666 /var/run/docker.sock
