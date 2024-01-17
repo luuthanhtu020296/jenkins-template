7fde8246e3b46d0c5dd4817351de75330823ce12
luuthanhtu <hoangtusocolakuteboy@gmail.com>
2024-01-17 20:51:49 +0700

    Create install-docker-amazon.sh

---
+++ linux_install_guilde_docker/install-docker-amazon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo dnf update -y
+sudo dnf install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+newgrp docker
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+sudo usermod -aG docker $USER
